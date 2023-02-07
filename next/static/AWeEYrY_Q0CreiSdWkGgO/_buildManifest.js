24857fd21692af6738af4915b577a58e22390ed4
balzae <114008545+balzae@users.noreply.github.com>
2023-02-07 18:08:27 +0100

    Delete next directory

--- next/static/AWeEYrY_Q0CreiSdWkGgO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-71617f1b84b24287.js"],"/404":["static/chunks/pages/404-2a102171785a3a9a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
